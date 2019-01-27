f0eb59b1ef776e69899e1db7939b48ef3b80cc25
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-01-27 18:20:23 +0000

    bulding out controllers and views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,32 @@ get '/signup' do
   end
 end
 
+post '/signup' do
+  if params[:username] == "" || params[:email]== "" || params[:password]==""
+    redirect to '/signup'
+  else
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    session[:user_id] = @user.id
+    redirect to '/'
+  end
+end
+
+get '/login' do
+  if !logged_in?
+    erb:'user/login'
+  end
+end
+
+get '/logout' do
+if logged_in?
+  session.clear
+  redirect to '/login'
+else
+  redirect to '/'
+end
+
+end
+
+
 end
