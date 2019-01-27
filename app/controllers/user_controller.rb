f02d2e6b7cc8097932443b72a2bdff787eb9bb4d
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-01-27 18:44:26 +0000

    create POST login route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,6 +23,18 @@ get '/login' do
   end
 end
 
+post '/login' do
+  user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+    redirect to '/'
+  else
+    redirect to '/signup'
+  end
+
+end
+
+
 get '/logout' do
 if logged_in?
   session.clear
