672b9ee64bedf9ccd1b4bf3c75e290500ef3e215
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-01-28 15:40:46 +0000

    fix log in issue with login view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,7 +24,7 @@ get '/login' do
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:username])
+  @user = User.find_by(:email => params[:email])
    if @user && @user.authenticate(params[:password])
      session[:user_id] = @user.id
     redirect to '/'
