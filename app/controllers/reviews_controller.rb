8734040685c5d66ff94197b78a04d77a1f012487
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-01-28 22:18:32 +0000

    create post form route in review controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,28 @@ class ReviewsController < ApplicationController
     if logged_in?
       erb :'reviews/create_review'
     else 
-      redirect to '/signup'
+      redirect to '/'
     end
     
+  end
+  
+  post '/reviews' do
+    if logged_in?
+      if params[:product_name]== "" || params[:content]=="" || params[:rating].to_i > 10 || params[:rating].to_i < 0
+        redirect to '/reviews/create_review'
+      else
+        @review.product_name = params[:product_name]
+        @review.content = params[:content]
+        @review.rating = params[:rating]
+        if @review.save
+          redirect to "/reviews/#{@review.id}"
+        else 
+          redirect to "/reviews/create_review"
+        end
+      end
+    else
+      redirect to '/'
+    end
   end
   
   
